0f792b23cb299ebcb8e6b66321d21a22ed6c7af1
Vasco Costa <vascomacosta@gmail.com>
2024-06-01 20:27:25 +0100

    Add feeds command

--- src/commands.rs
+++ src/commands.rs
@@ -1,20 +1,77 @@
 use chrono::Utc;
-use std::{error::Error, process::Command};
+use feed_rs::parser;
+use futures::{future::join_all, Future};
+use std::{collections::HashMap, error::Error, process::Command};
 
 pub trait BotCommand {
-    fn execute(&self) -> Result<String, Box<dyn Error>>;
+    fn execute(&self) -> impl Future<Output = Result<String, Box<dyn Error>>>;
     fn usage(&self) -> String;
 }
 
 pub struct DateCommand;
 
 impl BotCommand for DateCommand {
-    fn execute(&self) -> Result<String, Box<dyn Error>> {
+    async fn execute(&self) -> Result<String, Box<dyn Error>> {
         Ok(Utc::now().to_rfc2822())
     }
 
     fn usage(&self) -> String {
         String::from("Show current time")
+    }
+}
+
+pub struct FeedsCommand<'a> {
+    pub urls: &'a Option<HashMap<String, String>>,
+}
+
+impl<'a> FeedsCommand<'a> {
+    pub fn new(urls: &'a Option<HashMap<String, String>>) -> Self {
+        Self { urls }
+    }
+}
+
+impl<'a> BotCommand for FeedsCommand<'a> {
+    async fn execute(&self) -> Result<String, Box<dyn Error>> {
+        let mut output = String::new();
+        let feed_urls = self.urls.as_ref().ok_or("Could not find feed URLs")?;
+        let mut futures = Vec::new();
+
+        for (_, url) in feed_urls.iter() {
+            let client = reqwest::Client::new();
+            futures.push(client.get(url).header("User-Agent", "seribot").send());
+        }
+
+        let results = join_all(futures).await;
+
+        for result in results {
+            let feed_text = result?.text().await?;
+            let feed = parser::parse(feed_text.as_bytes())?;
+            let entries = feed
+                .entries
+                .iter()
+                .map(|e| {
+                    let title = match e.title.to_owned() {
+                        Some(title) => title.content,
+                        None => String::from("N/A"),
+                    };
+                    let summary = match e.summary.clone() {
+                        Some(summary) => summary.content,
+                        None => String::from("N/A"),
+                    };
+
+                    format!("{}\r\n\r\n{}", title, summary)
+                })
+                .collect::<Vec<String>>()
+                .join("\r\n\r\n-----\r\n\r\n");
+
+            output = format!("{}\r\n{}\r\n", output, entries);
+        }
+
+        Ok(output)
+    }
+
+    fn usage(&self) -> String {
+        String::from("Show RSS/Atom feeds")
     }
 }
 
@@ -33,7 +90,7 @@ impl ShellCommand {
 }
 
 impl BotCommand for ShellCommand {
-    fn execute(&self) -> Result<String, Box<dyn Error>> {
+    async fn execute(&self) -> Result<String, Box<dyn Error>> {
         match Command::new(self.name.clone())
             .args(self.args.clone().unwrap_or_default())
             .output()
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod commands;
 mod config;
 
-use commands::{BotCommand, DateCommand, ShellCommand};
+use commands::{BotCommand, DateCommand, FeedsCommand, ShellCommand};
 use config::Config;
 use ihex::Record;
 use std::{
@@ -11,20 +11,27 @@ use std::{
     time::Duration,
 };
 
-fn parse_command(line: String) -> Result<Box<dyn BotCommand>, Box<dyn Error>> {
+enum Commands {
+    Date,
+    Feeds,
+    Shell(String, Option<Vec<String>>),
+}
+
+fn parse_command(line: String) -> Result<Commands, Box<dyn Error>> {
     let mut parts = line.split_whitespace();
     let name = parts.next().ok_or("No command name")?.to_owned();
     let args: Vec<String> = parts.map(|a| a.to_owned()).collect();
     let args = if args.is_empty() { None } else { Some(args) };
 
     match name.as_str() {
-        "date" => Ok(Box::new(DateCommand {})),
-        "feeds" => todo!(),
-        _ => Ok(Box::new(ShellCommand::new(&name, args))),
+        "date" => Ok(Commands::Date),
+        "feeds" => Ok(Commands::Feeds),
+        _ => Ok(Commands::Shell(name, args)),
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::read("config.toml")?;
     let port = serialport::new(&config.serial.port, config.serial.baud)
         .data_bits(config.serial.data_bits)
@@ -43,7 +50,24 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match reader.read_line(&mut line) {
                     Ok(_) => {
                         let command = parse_command(line.clone())?;
-                        let output = format!("{}\r\n", command.execute()?);
+                        let output = match command {
+                            Commands::Date => {
+                                let command = DateCommand {};
+
+                                format!("{}\r\n", command.execute().await?)
+                            }
+                            Commands::Feeds => {
+                                let command = FeedsCommand::new(&config.feeds);
+
+                                format!("{}\r\n", command.execute().await?)
+                            }
+                            Commands::Shell(name, args) => {
+                                let command = ShellCommand::new(&name, args);
+
+                                format!("{}\r\n", command.execute().await?)
+                            }
+                        };
+
                         let chunks = output.as_bytes().chunks(255);
                         let mut records: Vec<Record> = Vec::new();
                         let mut offset = 0;
